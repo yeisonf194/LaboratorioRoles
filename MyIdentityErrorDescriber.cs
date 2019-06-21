c57d7070eb4a0052ce3ddca688da1b022bf408e6
yeisonf194 <yeisonf19@gmail.com>
2019-06-21 15:38:07 -0500

    Initial commit

---
+++ MyIdentityErrorDescriber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class MyIdentityErrorDescriber : IdentityErrorDescriber
+{
+	public override IdentityError ConcurrencyFailure()
+	{
+        return base.ConcurrencyFailure();
+
+    }
+    public override IdentityError DefaultError()
+    {
+        return base.DefaultError();
+    }
+    public override IdentityError DuplicateEmail(string email)
+    {
+        return new IdentityError
+        {
+            Code = nameof(DuplicateEmail),
+            Description = "El email ya se encuentra registrado en la base de datos"
+        };
+    }
+    public override IdentityError PasswordRequiresNonAlphanumeric()
+    {
+        return new IdentityError
+        {
+            Code = nameof(PasswordRequiresNonAlphanumeric),
+            Description = "Se requiere al menos un caracter especial"
+        };
+    }
+}
